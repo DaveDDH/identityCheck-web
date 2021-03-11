8ead70f109a70d45e6c199f64368b090b07bcbc1
David <l.david.dominguez.12@gmail.com>
2021-03-11 17:45:00 -0500

    primera version

--- main.py
+++ main.py
@@ -7,5 +7,17 @@ app = Flask(__name__, static_url_path='')
 def content():
   return render_template('index.html')
 
+@app.route('/signin')
+def contentSignIn():
+  return render_template('signin.html')
+
+@app.route('/signup')
+def contentSignUp():
+  return render_template('signup.html')
+
+@app.route('/main')
+def contentMain():
+  return render_template('main.html')
+
 port = int(os.environ.get("PORT", 33507))
 app.run(host='0.0.0.0', port=port)
