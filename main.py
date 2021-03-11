3ce76b9a26319a594f5c093a96a22bc654a2ff8c
David <l.david.dominguez.12@gmail.com>
2021-03-11 15:18:59 -0500

    cambio

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 from flask import Flask, request, render_template
 
+import os
+ON_HEROKU = os.environ.get('ON_HEROKU')
+if ON_HEROKU:
+    # get the heroku port 
+    port = int(os.environ.get("PORT", 17995))  # as per OP comments default is 17995
+else:
+    port = 3000
+
 app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def content():
   return render_template('index.html')
 
-app.run()
+app.run(port=port)
